ff12288016dafac56a8cbe9ed8517bee459772ca
wassim93 <wassim.benhassen@esprit.tn>
2022-01-12 10:44:22 +0000

    styling home conatainer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Home from "./components/Home";
 
 const App = () => {
   return (
     <div className="app">
       <Header />
+      <Home />
     </div>
   );
 };
